27b0b771c2b2c89c9a98ab7dcfd747f130e05f0d
JOS6666 <jose.caamano@alumno.iplacex.cl>
2025-11-23 20:45:22 -0300

    Carga de proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jrcs.app',
+  appName: 'jrcs-taller',
+  webDir: 'build'
+};
+
+export default config;
